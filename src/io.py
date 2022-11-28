93246f52a78a3fc6249ab0f14c8f60136794112c
Ben Magyar <bmagyar@etsy.com>
2022-11-28 16:12:59 -0500

    initial setup

---
+++ src/io.py
@@ -0,0 +1,12 @@
+import os
+
+def clean(lines):
+    return [line.rstrip() for line in lines if not line.isspace()]
+
+def read_example(day):
+    with open(os.path.join(os.getcwd(), "examples", day + ".txt")) as file:
+        return clean(file.readlines())
+
+def read_input(day):
+    with open(os.path.join(os.getcwd(), "inputs", day + ".txt")) as file:
+        return clean(file.readlines())
